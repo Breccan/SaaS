586dd83a0640ab7cbf53e9ee9afe2960796d3f2d
Breccan <jalnyfen@gmail.com>
2011-06-07 16:09:48 +1200

    Admin skeletons

---
+++ app/controllers/saas/admin/accounts_controller.rb
@@ -0,0 +1,11 @@
+module Saas
+  module Admin
+    class AccountsController < Saas::ApplicationController
+
+      def index
+
+      end
+
+    end
+  end
+end
---
+++ app/controllers/saas/admin/subscriptions_controller.rb
@@ -0,0 +1,31 @@
+module Saas
+  module Admin
+    class SubscriptionsController < Saas::ApplicationController
+
+      def index
+
+      end
+
+      def new
+
+      end
+
+      def create
+
+      end
+
+      def update
+
+      end
+
+      def edit
+
+      end
+
+      def destroy
+
+      end
+
+    end
+  end
+end
